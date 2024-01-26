678cc0768fbd4d4e668f2672b44359ca8b3a260f
Bithe <farzana.hossain5@gmail.com>
2024-01-26 06:24:07 +0600

    avg

---
+++ avg.js
@@ -0,0 +1,9 @@
+var math =75.25;
+var bio = 65;
+var che =80;
+var phy = 35.45;
+var bangla = 99.50;
+var total = math+bio+che+phy+bangla;
+var avg = total/5;
+console.log(avg);
+console.log(avg.toFixed(2));
