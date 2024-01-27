91746bc1daf8517a4dced90c24c0540746218e49
Bithe <farzana.hossain5@gmail.com>
2024-01-27 19:47:52 +0600

    av

--- avg.js
+++ avg.js
@@ -7,3 +7,14 @@ var total = math+bio+che+phy+bangla;
 var avg = total/5;
 console.log(avg);
 console.log(avg.toFixed(2));
+
+
+// reminder
+
+var num = 119;
+console.log(num%5);
+
+const a = isNaN('11');
+
+const b = isNaN(2-10);
+console.log(a,b);
