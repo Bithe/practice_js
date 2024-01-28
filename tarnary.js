60d0914f31f4f48f5afbec87df3512d8fe46bd72
Bithe <farzana.hossain5@gmail.com>
2024-01-29 05:20:57 +0600

    ternaryyy

--- tarnary.js
+++ tarnary.js
@@ -1,7 +1,21 @@
 let price = 500;
 const isLeader = true;
 
+// price = isLeader === true ? 0 : price + 100;
 
-price = isLeader === true ? 0 : price+100 ;
+// console.log(price);
 
+// if (isLeader === true) {
+//   if (price > 1000) {
+//     price = price / 2;
+//   } else {
+//     price = 0;
+//   }
+// } else {
+//   price = price + 100;
+// }
+
+price = isLeader === true ? 
+        price > 1000 ? price / 2 : 0 
+    : price + 100;
 console.log(price);
