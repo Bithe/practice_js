bb891e432590fbdbdfaefcc881f8e272c1a4469d
Bithe <farzana.hossain5@gmail.com>
2024-01-31 07:11:10 +0600

    task

--- loop.js
+++ loop.js
@@ -1,5 +1,18 @@
-const numbers =[2, 3, 4, 39, 84];
+// const numbers =[2, 3, 4, 39, 84];
 
-for(const num of numbers){
-    console.log(num);
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+let num = 1;
+let sum =0;
+
+while(num <= 5){
+    // console.log(num);
+    sum+=num;
+    num++;
+    // console.log(sum);
+
 }
+
+console.log(sum);
---
+++ task.js
@@ -0,0 +1,8 @@
+const burger = 600;
+
+if(burger>=500){
+    console.log('free drinks');
+}
+else{
+    console.log(' 30tk');
+}
