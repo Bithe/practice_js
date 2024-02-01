2acde26803fa6c313165080b72a946ab4fd4082b
Bithe <farzana.hossain5@gmail.com>
2024-02-01 09:18:12 +0600

    rev

--- func.js
+++ func.js
@@ -1,15 +1,15 @@
 // const score =70;
 
 // function calculateGrade(score){
- 
+
 //     if( score>=90 && score<=100){
 //         console.log('A');
 //     }
-     
+
 //    else if( score>=80 && score<=89){
 //         console.log('B');
 //     }
-     
+
 //     else if( score>=70 && score<=79){
 //         console.log('C');
 //     }
@@ -20,16 +20,22 @@
 // str[0] = "h";
 // console.log(str);
 
+// let n =0;
+// while(n<15){
+//     n++;
+//     if( n % 5 !== 0){
+//         continue;
+//     }
+//     console.log(n)
+// }
 
-let n =0;
-while(n<15){
-    n++;
-    if( n % 5 !== 0){
-        continue;
-    }
-    console.log(n)
+let sentence = "rayaan hossain";
+
+let rev = '';
+
+for (let i = 0; i <= sentence.length; i++) {
+  // console.log(i);
+
+   const letter = sentence[i]; // rev= letter + rev;
+  console.log(letter);
 }
-
-
-
-
---
+++ reverse.js
@@ -0,0 +1,16 @@
+let sentence ='devloper running';
+
+let rev=''
+
+for( i=0; i<sentence.length; i++){
+    // console.log(sentence[i]);
+    let letter = sentence[i];
+    // console.log(letter);
+    rev = letter+rev;
+    
+}
+
+console.log(rev);
+
+
+
