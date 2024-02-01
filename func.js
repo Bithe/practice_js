6101a88f7e9c84084b0b65cc407145b70af414dc
Bithe <farzana.hossain5@gmail.com>
2024-02-01 06:22:10 +0600

    tas

--- func.js
+++ func.js
@@ -1,24 +1,34 @@
-const score =70;
+// const score =70;
 
-function calculateGrade(score){
+// function calculateGrade(score){
  
-    if( score>=90 && score<=100){
-        console.log('A');
-    }
+//     if( score>=90 && score<=100){
+//         console.log('A');
+//     }
      
-   else if( score>=80 && score<=89){
-        console.log('B');
-    }
+//    else if( score>=80 && score<=89){
+//         console.log('B');
+//     }
      
-    else if( score>=70 && score<=79){
-        console.log('C');
-    }
-}
-const grade = calculateGrade(score);
+//     else if( score>=70 && score<=79){
+//         console.log('C');
+//     }
+// }
+// const grade = calculateGrade(score);
 
-const str = "Hello";
-str[0] = "h";
-console.log(str);
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+
+let n =0;
+while(n<15){
+    n++;
+    if( n % 5 !== 0){
+        continue;
+    }
+    console.log(n)
+}
 
 
 
