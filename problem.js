70461b1cd5b24b34b77e18ad77dc15612269756a
Bithe <farzana.hossain5@gmail.com>
2024-02-05 10:48:05 +0600

    odd

---
+++ problem.js
@@ -0,0 +1,31 @@
+function inchtoFeet(inch){
+const feet = inch/12;
+const remaining = inch %12;
+const result = feet+ ' ft ' + remaining + ' inch';
+
+return result;
+}
+
+console.log(inchtoFeet(75));
+
+
+function odd_avg(numbers){
+
+    const odd = [];
+    let sum = 0;
+    for(const num of numbers){
+        if(num % 2 !== 0){
+            odd.push(num);
+            sum += num;
+            }
+    }
+    const count = odd.length;
+    const avg = sum / count;
+    return avg;
+
+}
+
+const array = [2, 3, 4, 5, 3];
+
+const total = odd_avg(array);
+console.log(total);
