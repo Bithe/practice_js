28fb80e503e77219dfb6e5130172d4081afd8531
Bithe <farzana.hossain5@gmail.com>
2024-02-06 04:38:21 +0600

    podd

--- basic.js
+++ basic.js
@@ -22,9 +22,19 @@ console.log(cubeNumber('h'));
 // 
 
 function matchFinder(string1, string2){
+
+    if(typeof(string1 || string2 ) !== 'string'){
+        return 'enter a string';
+    }
     const match = string1.includes(string2);
     return match;
 }
 
 console.log(matchFinder('Joh', 'jo'));
+console.log(matchFinder('joh', 'jo'));
+console.log(matchFinder('Joh', '2'));
+console.log(matchFinder(3, 'jo'));
+
+// 
+
 
