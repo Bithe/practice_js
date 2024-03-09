2fa3796baece5dfbedae7b45f4e81106271bfa2a
Bithe <farzana.hossain5@gmail.com>
2024-03-09 13:40:32 +0600

    distuctr

--- basicConcep.js
+++ basicConcep.js
@@ -11,3 +11,27 @@ const person ={
 
 const { address: {zilla: district} } = person;
 console.log(district);
+
+const numbers =[1,5,7,9];
+
+
+const a = numbers.forEach(num => num);
+console.log(a);
+const b = numbers.map(num=>num+1);
+console.log(b);
+console.log(numbers);
+
+
+const x = numbers.find(item => item ===5);
+console.log(x);
+
+const y = numbers.filter(item => item%2 ===0);
+console.log(y);
+
+
+const r = numbers.reduce( (preValue, currentValue) =>{
+    return preValue+currentValue;
+},0 );
+
+console.log(r);
+
